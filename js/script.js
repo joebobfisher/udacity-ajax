4fabf2bc78c8842a63eb1510a961dacb2a449066
Joe Fisher <joe.fisher@distek.com>
2018-02-09 22:43:34 -0600

    NYT Solution.

--- js/script.js
+++ js/script.js
@@ -17,6 +17,20 @@ function loadData() {
     var address = street + ', ' + city;
     $body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+ address +'&key=AIzaSyAogT6qF8foBqImJlu5NYdwWcmBUjF8ga0">')
 
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=f4a21a9e6af743e8af51eb60b31f67d2';
+    $.getJSON(nytimesUrl, function(data){
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
     return false;
 };
 
